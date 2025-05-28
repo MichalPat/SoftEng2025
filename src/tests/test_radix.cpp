005ef01c876486fd4aabd9589aff255a33918170
Michał <michal.patkowski13@gmail.com>
2025-05-28 22:17:29 +0200

    test_radix.cpp dodany

--- src/tests/test_radix.cpp
+++ src/tests/test_radix.cpp
@@ -1,15 +1,30 @@
 
 #include <gtest/gtest.h>
-#include "Sorter.h"
-#include "SortData.h"
+#include "Radix.h"
+#include <vector>
+#include <algorithm>
 
-TEST(test_radix, AddFunction)
-{
-    SortError error = SE_SUCCESS;
+bool isSorted(const std::vector<int>& vec) {
+    return std::is_sorted(vec.begin(), vec.end());
+}
 
-    // add your test here
 
-    ASSERT_EQ(error, SE_SUCCESS);
+TEST(RadixSortTest, SortBase10) {
+    std::vector<int> input = {233, 3, 87, 44, 255, 111, 11, 32, 1};
+    std::vector<int> result = radixSort(input, 10);
+    ASSERT_TRUE(isSorted(result));
+}
+
+TEST(RadixSortTest, SortBase16) {
+    std::vector<int> input = {233, 3, 87, 44, 255, 111, 11, 32, 1};
+    std::vector<int> result = radixSort(input, 16);
+    ASSERT_TRUE(isSorted(result));
+}
+
+TEST(RadixSortTest, SortEmptyVector) {
+    std::vector<int> input = {};
+    std::vector<int> result = radixSort(input, 10);
+    ASSERT_TRUE(result.empty());
 }
 
 int main(int argc, char** argv)
